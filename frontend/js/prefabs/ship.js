6ace215a9c7e8dba9afcf03d6e07e73b51676b14
Matt Mulhern <mmulhern@proofpoint.com>
2017-01-21 16:56:24 +0000

    whitespace fixes
    Signed-off-by: Matt Mulhern <mmulhern@proofpoint.com>

--- frontend/js/prefabs/ship.js
+++ frontend/js/prefabs/ship.js
@@ -2,9 +2,9 @@
 'use strict';
 
 var Ship = function(state, x, y, controls) {
-    // instanciate object 
+    // instantiate object
     Phaser.Sprite.call(this, state.game, x, y, 'ship');
-    // constante
+    // constants
     this.x = x;
     this.y = y;
     this.controls = controls;
@@ -14,11 +14,10 @@ var Ship = function(state, x, y, controls) {
     state.game.physics.enable(this, Phaser.Physics.ARCADE);
     this.body.drag.set(100);
     this.body.maxVelocity.set(200);
-    
+
     // add to canvas and log
     state.game.add.existing(this);
 
-    // TODO: move to Ship.addBullets?
     this.bulletTime = 0;
     this.bullets = this.game.add.group();
     this.bullets.enableBody = true;
@@ -26,7 +25,7 @@ var Ship = function(state, x, y, controls) {
     this.bullets.createMultiple(40, 'bullet');
     this.bullets.setAll('anchor.x', 0.5);
     this.bullets.setAll('anchor.y', 0.5);
-    
+
     console.log(this);
 };
 
@@ -34,22 +33,23 @@ Ship.prototype = Object.create(Phaser.Sprite.prototype);
 
 Ship.prototype.constructor = Ship;
 
-  // prefab specific frame update code
 Ship.prototype.update = function() {
-    if (this.controls.forward.isDown)
-    { this.game.physics.arcade.accelerationFromRotation(this.rotation, 200, this.body.acceleration); }
-    else
-    { this.body.acceleration.set(0); }
 
-    if (this.controls.left.isDown)
-    { this.body.angularVelocity = -300; }
-    else if (this.controls.right.isDown)
-    { this.body.angularVelocity = 300; }
-    else
-    { this.body.angularVelocity = 0; }
+    if (this.controls.forward.isDown) {
+        this.game.physics.arcade.accelerationFromRotation(this.rotation, 200, this.body.acceleration);
+    }
 
-    if (this.controls.fire1.isDown)  // TOO: replace with callback for single bullet per press
-    { this.fireBullet(); }
+    else this.body.acceleration.set(0);
+
+    if (this.controls.left.isDown) {
+        this.body.angularVelocity = -300;
+    }
+    else if (this.controls.right.isDown) {
+        this.body.angularVelocity = 300;
+    }
+    else this.body.angularVelocity = 0;
+
+    if (this.controls.fire1.isDown) this.fireBullet();
 };
 
 Ship.prototype.fireBullet = function () {
@@ -69,32 +69,5 @@ Ship.prototype.fireBullet = function () {
     }
 
 }
+
 module.exports = Ship;
-/*
-sprite.cursors = new Object();
-sprite.cursors.forward = game.input.keyboard.addKey(Phaser.Keyboard.W);
-sprite.cursors.left = game.input.keyboard.addKey(Phaser.Keyboard.A);
-sprite.cursors.right = game.input.keyboard.addKey(Phaser.Keyboard.D);
-
-
-sprite.myfire = function fireBullet () {
-
-    if (game.time.now > bulletTime)
-    {
-        bullet = bullets.getFirstExists(false);
-
-        if (bullet)
-        {
-            bullet.reset(sprite.body.x + 16, sprite2.body.y + 16);
-            bullet.lifespan = 2000;
-            bullet.rotation = sprite.rotation;
-            game.physics.arcade.velocityFromRotation(sprite.rotation, 400, bullet.body.velocity);
-            bulletTime = game.time.now + 50;
-        }
-    }
-
-}
-sprite.cursors.fire = game.input.keyboard.addKey(Phaser.Keyboard.Q);
-sprite.cursors.fire.onDown.add(sprite.myfire);
-
-*/
