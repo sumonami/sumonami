0a5c8e471092e9020c6fc26c4e290d0a93af70ee
Matt Mulhern <mmulhern@proofpoint.com>
2017-01-21 17:36:09 +0000

    removing dead code in playstate
    Signed-off-by: Matt Mulhern <mmulhern@proofpoint.com>

--- frontend/js/states/playfield.js
+++ frontend/js/states/playfield.js
@@ -15,23 +15,6 @@ PlayfieldState.prototype.preload = function() {
 PlayfieldState.prototype.create = function() {
     console.log('PLAY FIELD');
 
-    //var Bird = function(game, x, y, frame) {  
-     // The super call to Phaser.Sprite
-     // Phaser.Sprite.call(this, game, x, y, ‘bird’, frame);
-
-     // // set the sprite’s anchor to the center
-     // this.anchor.setTo(0.5, 0.5);
-
-     // // add and play animations
-     // this.animations.add(‘flap’);
-     // this.animations.play(‘flap’, 12, true);
-
-    //
-    //this.bird = this.add.sprite(200,5,’bird’);  
-
-    //Ship.prototype.constructor = function(game, x, y, frame, controls) {
-
-
     //  This will run in Canvas mode, so let's gain a little speed and display
     this.game.renderer.clearBeforeRender = false;
     this.game.renderer.roundPixels = true;
@@ -56,59 +39,9 @@ PlayfieldState.prototype.create = function() {
     this.ships.add(new Ship(this, 100, 100, this.cursors));
 
     console.log(this.ships);
-   /*
-    // Our ships bullets
-    this.bullets = this.game.add.group();
-    this.bullets.enableBody = true;
-    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-    //  All 40 of them
-    this.bullets.createMultiple(40, 'bullet');
-    this.bullets.setAll('anchor.x', 0.5);
-    this.bullets.setAll('anchor.y', 0.5);
-
-    this.bulletTime = 0;
-    this.sprite = this.game.add.sprite(300, 300, 'ship');
-    this.sprite.anchor.set(0.5);
-    //  and its physics settings
-    this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
-    this.sprite.body.drag.set(100);
-    this.sprite.body.maxVelocity.set(200);
-
-    //  Game input
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-    this.game.input.keyboard.addKeyCapture([ Phaser.Keyboard.SPACEBAR ]);
-    */
 };
 
 PlayfieldState.prototype.update = function() {
-    /*
-    if (this.cursors.up.isDown)
-    {
-        this.game.physics.arcade.accelerationFromRotation(this.sprite.rotation, 200, this.sprite.body.acceleration);
-    }
-    else
-    {
-        this.sprite.body.acceleration.set(0);
-    }
-
-    if (this.cursors.left.isDown)
-    {
-        this.sprite.body.angularVelocity = -300;
-    }
-    else if (this.cursors.right.isDown)
-    {
-        this.sprite.body.angularVelocity = 300;
- this   }
-    else
-    {
-        this.sprite.body.angularVelocity = 0;
-    }
-
-    if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
-    {
-        this.fireBullet();
-    }
-    */
 };
 
 PlayfieldState.prototype.fireBullet = function() {
