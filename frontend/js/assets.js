0acee2a3cde956b6ac2198a82f1288f15378ac68
Daniel Porter <dporter@proofpoint.com>
2017-01-22 15:13:48 +0000

    Add all audio files and definitions

--- frontend/js/assets.js
+++ frontend/js/assets.js
@@ -37,6 +37,75 @@ var assets = {
     ],
     // Audio files to load
     sounds: [
+        ['bgm-applause', ['audio/bgm-applause.mp3',
+                          'audio/bgm-applause.opus']
+        ],
+        ['bgm-playfield', ['audio/bgm-playfield.mp3',
+                           'audio/bgm-playfield.opus']
+        ],
+        ['bgm-title', ['audio/bgm-title.mp3',
+                       'audio/bgm-title.opus']
+        ],
+        ['sfx-bounce', ['audio/sfx-bounce.mp3',
+                        'audio/sfx-bounce.opus']
+        ],
+        ['sfx-sausagehit', ['audio/sfx-sausagehit.mp3',
+                            'audio/sfx-sausagehit.opus']
+        ],
+        ['sfx-sausagethrow', ['audio/sfx-sausagethrow.mp3',
+                              'audio/sfx-sausagethrow.opus']
+        ],
+        ['sfx-slashsoft', ['audio/sfx-slashsoft.mp3',
+                           'audio/sfx-slashsoft.opus']
+        ],
+        ['sfx-splashhard', ['audio/sfx-splashhard.mp3',
+                            'audio/sfx-splashhard.opus']
+        ],
+        ['sfx-startgame', ['audio/sfx-startgame.mp3',
+                            'audio/sfx-startgame.opus']
+        ],
+        ['sfx-washed-last', ['audio/sfx-washed-last.mp3',
+                             'audio/sfx-washed-last.opus']
+        ],
+        ['voice-gameover1', ['audio/voice-gameover1.mp3',
+                             'audio/voice-gameover1.opus']
+        ],
+        ['voice-gameover2', ['audio/voice-gameover2.mp3',
+                             'audio/voice-gameover2.opus']
+        ],
+        ['voice-gameover3', ['audio/voice-gameover3.mp3',
+                             'audio/voice-gameover3.opus']
+        ],
+        ['voice-ohNo', ['audio/voice-ohNo.mp3',
+                        'audio/voice-ohNo.opus']
+        ],
+        ['voice-pleasewait', ['audio/voice-pleasewait.mp3',
+                              'audio/voice-pleasewait.opus']
+        ],
+        ['voice-pleasewait2', ['audio/voice-pleasewait2.mp3',
+                               'audio/voice-pleasewait2.opus']
+        ],
+        ['voice-seeyoulater', ['audio/voice-seeyoulater.mp3',
+                               'audio/voice-seeyoulater.opus']
+        ],
+        ['voice-seeyoulater2', ['audio/voice-seeyoulater2.mp3',
+                                'audio/voice-seeyoulater2.opus']
+        ],
+        ['voice-stupidForeigner', ['audio/voice-stupidForeigner.mp3',
+                                   'audio/voice-stupidForeigner.opus']
+        ],
+        ['voice-sumonami', ['audio/voice-sumonami.mp3',
+                            'audio/voice-sumonami.opus']
+        ],
+        ['voice-sumonami2', ['audio/voice-sumonami2.mp3',
+                             'audio/voice-sumonami2.opus']
+        ],
+        ['voice-sumonami3', ['audio/voice-sumonami3.mp3',
+                             'audio/voice-sumonami3.opus']
+        ],
+        ['voice-sumonami4', ['audio/voice-sumonami4.mp3',
+                             'audio/voice-sumonami4.opus']
+        ]
     ]
 };
 
