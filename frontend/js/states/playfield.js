2785e34de4d4dad34f0a0eaa52e0ad5700b92468
Daniel Porter <dporter@proofpoint.com>
2017-01-22 13:53:01 +0000

    8 player support

--- frontend/js/states/playfield.js
+++ frontend/js/states/playfield.js
@@ -72,56 +72,108 @@ PlayfieldState.prototype.createPlayers = function(numPlayers) {
     var state = this;
     var players = {
         player1: {
-            initLoc: [350, 400],
+            initLoc: [350, 200],
             sprite: 'player1',
             controls: {
-                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.W),
-                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.S),
-                left: state.game.input.keyboard.addKey(Phaser.Keyboard.A),
-                right: state.game.input.keyboard.addKey(Phaser.Keyboard.D),
-                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.Q),
-                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.E)
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.TWO),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.Q),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.ONE),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.W),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.F1),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.F2)
             },
             image: 'player1'
         },
         player2: {
-            initLoc: [550, 400],
+            initLoc: [550, 200],
             sprite: 'player2',
             controls: {
-                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.I),
-                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.K),
-                left: state.game.input.keyboard.addKey(Phaser.Keyboard.J),
-                right: state.game.input.keyboard.addKey(Phaser.Keyboard.L),
-                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.U),
-                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.O)
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.SIX),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.T),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.FIVE),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.Y),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.NINE),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.ZERO)
             },
             image: 'player2'
         },
         player3: {
-            initLoc: [750, 400],
+            initLoc: [750, 200],
             sprite: 'player3',
             controls: {
-                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.T),
-                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.G),
-                left: state.game.input.keyboard.addKey(Phaser.Keyboard.F),
-                right: state.game.input.keyboard.addKey(Phaser.Keyboard.H),
-                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.R),
-                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.Y)
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.S),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.Z),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.A),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.X),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.F3),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.F4)
             },
             image: 'player3'
         },
         player4: {
-            initLoc: [950, 400],
+            initLoc: [950, 200],
             sprite: 'player4',
             controls: {
-                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.UP),
-                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
-                left: state.game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
-                right: state.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
-                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.SHIFT),
-                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.CONTROL)
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.H),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.B),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.G),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.N),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.MINUS),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.PLUS)
             },
             image: 'player4'
+        },
+        player5: {
+            initLoc: [350, 600],
+            sprite: 'player5',
+            controls: {
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.FOUR),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.E),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.THREE),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.R),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.F5),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.F6)
+            },
+            image: 'player5'
+        },
+        player6: {
+            initLoc: [550, 600],
+            sprite: 'player6',
+            controls: {
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.EIGHT),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.U),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.SEVEN),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.I),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.O),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.P)
+            },
+            image: 'player6'
+        },
+        player7: {
+            initLoc: [750, 600],
+            sprite: 'player7',
+            controls: {
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.F),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.C),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.D),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.V),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.F7),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.F8)
+            },
+            image: 'player7'
+        },
+        player8: {
+            initLoc: [950, 600],
+            sprite: 'player8',
+            controls: {
+                forward: state.game.input.keyboard.addKey(Phaser.Keyboard.K),
+                backward: state.game.input.keyboard.addKey(Phaser.Keyboard.M),
+                left: state.game.input.keyboard.addKey(Phaser.Keyboard.J),
+                right: state.game.input.keyboard.addKey(Phaser.Keyboard.COMMA),
+                fire1: state.game.input.keyboard.addKey(Phaser.Keyboard.OPEN_BRACKET),
+                fire2: state.game.input.keyboard.addKey(Phaser.Keyboard.CLOSED_BRACKET)
+            },
+            image: 'player8'
         }
     };
 
