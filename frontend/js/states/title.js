93cd70f27388022dc1b6c1961a88d8d6e7cecbaf
Michael Robinson <mrobinson@proofpoint.com>
2017-01-22 10:15:53 +0000

    Add glow effect and tween to control alpha on the title text

--- frontend/js/states/title.js
+++ frontend/js/states/title.js
@@ -19,6 +19,21 @@ TitleState.prototype.create = function() {
     this.startButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     this.startButton.onDown.add(this.startGame, this);
 
+    this.text.filters = [ this.game.add.filter('Glow')  ];
+
+    this.game.add.tween(this.text)
+            .to({ alpha: 1.0  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 0.1  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 1.0  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 0.1  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 1.0  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 1.0  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 0.1  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 1.0  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 0.1  }, 2000, Phaser.Easing.Linear.In)
+            .to({ alpha: 1.0  }, 2000, Phaser.Easing.Linear.In)
+            .start();
+
     function sleep(ms) {
           return new Promise(resolve => setTimeout(resolve, ms));
     }
@@ -34,5 +49,41 @@ TitleState.prototype.startGame = function () {
     console.log("starting!");
     this.state.start("PlayField");
 };
+
+Phaser.Filter.Glow = function (game) {
+        Phaser.Filter.call(this, game);
+
+    this.fragmentSrc = [
+                "precision lowp float;",
+                "varying vec2 vTextureCoord;",
+                "varying vec4 vColor;",
+                'uniform sampler2D uSampler;',
+
+                'void main() {',
+                    'vec4 sum = vec4(0);',
+                    'vec2 texcoord = vTextureCoord;',
+                    'for(int xx = -4; xx <= 4; xx++) {',
+                        'for(int yy = -3; yy <= 3; yy++) {',
+                            'float dist = sqrt(float(xx*xx) + float(yy*yy));',
+                            'float factor = 0.0;',
+                            'if (dist == 0.0) {',
+                                'factor = 2.0;',
+                            '} else {',
+                                'factor = 2.0/abs(float(dist));',
+                            '}',
+                            'sum += texture2D(uSampler, texcoord + vec2(xx, yy) * 0.002) * factor;',
+                        '}',
+                    '}',
+                    'gl_FragColor = sum * 0.025 + texture2D(uSampler, texcoord);',
+                '}'
+            
+    ];
+
+};
+
+Phaser.Filter.Glow.prototype = Object.create(Phaser.Filter.prototype);
+Phaser.Filter.Glow.prototype.constructor = Phaser.Filter.Glow;
+
+
 
 module.exports = TitleState;
