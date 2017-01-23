ed7ae1732267fe918d8da2c24e0cc5e7c5cd88d1
Daniel Porter <dporter@proofpoint.com>
2017-01-23 17:21:07 +0000

    Optimize audio

--- static/assets/audio/convert.sh
+++ static/assets/audio/convert.sh
@@ -2,6 +2,6 @@
 for filename in *.wav
 do
     name=${filename%.*}
-    opusenc --bitrate 96 ${name}.wav ${name}.opus
-    ffmpeg -i ${name}.wav -codec:a libmp3lame -qscale:a 2 ${name}.mp3
+    opusenc --bitrate 32 --framesize 60 --downmix-mono ${name}.wav ${name}.opus
+    ffmpeg -y -i ${name}.wav -codec:a libmp3lame -qscale:a 8 -ac 1 ${name}.mp3
 done
